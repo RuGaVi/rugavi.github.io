8ecd91d489b51789764fe573092ad839498b18e5
Vidisa <job.gargi@gmail.com>
2020-09-28 23:22:23 +0530

    Updates

---
+++ precache-manifest.dd2e79856b3565df2b67ee7ada79c609.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbc1eac6a08f057a0f971e80235d15c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0973a5e4765682e47f51",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b3efe1aec162c9c8f7a",
+    "url": "/static/js/2.d1ba5a21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1ba5a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0973a5e4765682e47f51",
+    "url": "/static/js/main.06829b60.chunk.js"
+  },
+  {
+    "revision": "b8a1a6411a1a052b70f0",
+    "url": "/static/js/runtime-main.9aba636d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
